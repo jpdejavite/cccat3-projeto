6fc8ac66b895249fa33e8c4d251f9fbc563dadac
Joao Araujo <jpdejavite@gmail.com>
2021-09-24 09:35:27 -0300

    refactor: cpf validator methods o private

--- src/validators/cpf.validator.ts
+++ src/validators/cpf.validator.ts
@@ -4,63 +4,67 @@ const DIGIT_1_BASE_MULTIPLIER = 10;
 const DIGIT_2_BASE_MULTIPLIER = 11;
 
 
+const calculateMod11 = (num: number): number => {
+  const remainder = num % 11;
+
+  return remainder < 2 ? 0 : 11 - remainder;
+};
+const calculateVerificationDigits = (onlyNumberString: string): string => {
+  let digito1Sum = 0;
+  let digito2Sum = 0;
+  const sizeWithoutVerificationDigits = onlyNumberString.length - 2;
+
+  // calculate both digits in one loop for efficiency
+  for (let index = 0; index < sizeWithoutVerificationDigits; index++) {
+    const num = parseInt(onlyNumberString[index], 10);
+    digito1Sum += (DIGIT_1_BASE_MULTIPLIER - index) * num;
+    digito2Sum += (DIGIT_2_BASE_MULTIPLIER - index) * num;
+  }
+
+
+  const dg1 = calculateMod11(digito1Sum);
+  digito2Sum += 2 * dg1;
+  const dg2 = calculateMod11(digito2Sum);
+
+  return `${dg1}${dg2}`;
+};
+
+const areAllDigitsRepeated = (onlyNumberString: string): boolean => {
+  let allDigitsAreEqual = true;
+  const firtsChar = onlyNumberString[0];
+  for (const stringChar of onlyNumberString) {
+    allDigitsAreEqual = allDigitsAreEqual && firtsChar === stringChar;
+  }
+
+  return allDigitsAreEqual;
+};
+
+const removeMask = (rawString: string): string => {
+  return rawString.replace(/[^0-9]/ig, '');
+};
+
+const isLengthValid = (rawString: string): boolean => {
+  if (rawString === '') {
+    return false;
+  }
+  return rawString.length === CPF_LENGTH || rawString.length === CPF_SIZE_WITH_MASK;
+};
+
 const CpfValidator = {
-  calculateMod11(num: number): number {
-    const remainder = num % 11;
-
-    return remainder < 2 ? 0 : 11 - remainder;
-  },
-  calculateVerificationDigits(onlyNumberString: string): string {
-    let digito1Sum = 0;
-    let digito2Sum = 0;
-    const sizeWithoutVerificationDigits = onlyNumberString.length - 2;
-
-    // calculate both digits in one loop for efficiency
-    for (let index = 0; index < sizeWithoutVerificationDigits; index++) {
-      const num = parseInt(onlyNumberString[index], 10);
-      digito1Sum += (DIGIT_1_BASE_MULTIPLIER - index) * num;
-      digito2Sum += (DIGIT_2_BASE_MULTIPLIER - index) * num;
-    }
-
-
-    const dg1 = CpfValidator.calculateMod11(digito1Sum);
-    digito2Sum += 2 * dg1;
-    const dg2 = CpfValidator.calculateMod11(digito2Sum);
-
-    return `${dg1}${dg2}`;
-  },
-  areAllDigitsRepeated(onlyNumberString: string): boolean {
-    let allDigitsAreEqual = true;
-    const firtsChar = onlyNumberString[0];
-    for (const stringChar of onlyNumberString) {
-      allDigitsAreEqual = allDigitsAreEqual && firtsChar === stringChar;
-    }
-
-    return allDigitsAreEqual;
-  },
-  removeMask(rawString: string): string {
-    return rawString.replace(/[^0-9]/ig, '');
-  },
-  isLengthValid(rawString: string): boolean {
-    if (rawString === '') {
-      return false;
-    }
-    return rawString.length === CPF_LENGTH || rawString.length === CPF_SIZE_WITH_MASK;
-  },
   validate(rawString: string): boolean {
-    if (!CpfValidator.isLengthValid(rawString)) {
+    if (!isLengthValid(rawString)) {
       return false;
     }
 
-    const onlyNumberString = CpfValidator.removeMask(rawString);
+    const onlyNumberString = removeMask(rawString);
 
-    if (CpfValidator.areAllDigitsRepeated(onlyNumberString)) {
+    if (areAllDigitsRepeated(onlyNumberString)) {
       return false;
     }
 
-    const calculateVerificationDigits = CpfValidator.calculateVerificationDigits(onlyNumberString);
+    const calculatedVerificationDigits = calculateVerificationDigits(onlyNumberString);
     const verificationDigits = onlyNumberString.substring(onlyNumberString.length - 2, onlyNumberString.length);
-    return verificationDigits === calculateVerificationDigits;
+    return verificationDigits === calculatedVerificationDigits;
   },
 };
 
