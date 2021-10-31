ab3bc416956371e33de18d479b4cf178938bf038
Joao Araujo <jpdejavite@gmail.com>
2021-10-31 15:09:54 -0300

    fix: multiple connections warning

--- src/infra/database/database-connection-adapter.ts
+++ src/infra/database/database-connection-adapter.ts
@@ -4,10 +4,15 @@ import DatabaseConnection from './database-connection';
 
 
 export default class DatabaseConnectionAdapter implements DatabaseConnection {
+  private static readonly pgpMap: Map<string, any> = new Map();
   pgp: any;
 
   constructor(url?: string) {
-    this.pgp = pgp()(url || 'postgres://postgres:123456@localhost:5432/app');
+    const connectionUrl = url || 'postgres://postgres:123456@localhost:5432/app';
+    if (!DatabaseConnectionAdapter.pgpMap.get(connectionUrl)) {
+      DatabaseConnectionAdapter.pgpMap.set(connectionUrl, pgp()(connectionUrl));
+    }
+    this.pgp = DatabaseConnectionAdapter.pgpMap.get(connectionUrl);
   }
 
   query(statement: string, params: any) {
